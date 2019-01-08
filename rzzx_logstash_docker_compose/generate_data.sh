1b1d02dc524c3c47a229edcd2a14492c6058911d
Joyner-Lu <623582960@qq.com>
2019-01-08 16:14:21 +0800

    修改配置文件

--- rzzx_logstash_docker_compose/generate_data.sh
+++ rzzx_logstash_docker_compose/generate_data.sh
@@ -1,5 +1,5 @@
 i=1
-while(($i<=10))
+while(($i<=1000000))
 do
   echo '20:15:52.189 [New I/O server worker #1-1] DEBUG c.f.d.r.e.s.header.HeartbeatHandler -  [DUBBO] Receive heartbeat response in thread New I/O server worker #1-1, dubbo version: 2.5.4-SNAPSHOT, current host: 10.10.0.99'>>./app.log
   i=$(($i+1))
